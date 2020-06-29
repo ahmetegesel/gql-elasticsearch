b3840eabc048a72fb35aaa2f697afaea1c079474
Ahmet Egesel <ahmetegesel@hotmail.com>
2020-06-29 23:53:55 +0200

    reconstructed mongodb setup in a more extendible fp way

--- repos/gql-api/lib/db/mongodb.js
+++ repos/gql-api/lib/db/mongodb.js
@@ -1,28 +1,54 @@
 import R from 'ramda';
 import { MongoClient, ObjectId } from 'mongodb';
 
-const localUrl = process.env.MONGO_NODE;
+const defaultToEmptyObject = R.defaultTo({});
 
-export const createClient = R.memoizeWith(
-  R.identity,
-  R.curry((url) => {
-    console.log('invoked');
-    return MongoClient.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  })
+const localUrl = 'mongodb://root:rootpassword@localhost:27017';
+
+export const DEFAULT_CLIENT_OPTIONS = {
+  url: localUrl,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+export const createClient = R.memoizeWith(R.identity, (options) => {
+  const finalOptions = R.mergeDeepRight(DEFAULT_CLIENT_OPTIONS, defaultToEmptyObject(options));
+  const { url } = finalOptions;
+
+  return MongoClient.connect(url, R.omit(['url'], finalOptions));
+});
+
+const useDatabase = R.curry((clientOptions, databaseName) =>
+  R.pipe(
+    () => createClient(clientOptions),
+    R.andThen((client) => client.db(databaseName))
+  )()
 );
 
-const useDatabase = R.curry((databaseName, client) => client.db(databaseName));
+const useCollection = R.curry((clientOptions, databaseName, collectionName) =>
+  R.pipe(
+    () => useDatabase(clientOptions, databaseName),
+    R.andThen((db) => db.collection(collectionName))
+  )()
+);
 
-const useCollection = R.curry((collectionName, db) => db.collection(collectionName));
+const useLocalDatabase = useDatabase(undefined);
 
-const useProductDatabase = useDatabase('products');
+const useProductsCollection = useCollection(R.__, 'products', 'products');
+
+const useProductsCollectionOnLocal = () => useProductsCollection(undefined); // if undefined, will use default options
+
+const useCollectionOnLocalProducts = useCollection(undefined, 'products');
+
+const useDatabaseOld = R.curry((databaseName, client) => client.db(databaseName));
+
+const useCollectionOld = R.curry((collectionName, db) => db.collection(collectionName));
+
+const useProductDatabase = useDatabaseOld('products');
 
 const useLocalProductDatabase = () => {
   return R.pipe(
-    () => createClient(localUrl),
+    () => createClient(),
     R.andThen((client) => useProductDatabase(client))
   )();
 };
@@ -30,10 +56,28 @@ const useLocalProductDatabase = () => {
 const findByObjectId = (id, collection) => collection.findOne(new ObjectId(id));
 
 useLocalProductDatabase().then(async (db) => {
-  const products = useCollection('products', db);
+  const products = useCollectionOld('products', db);
 
   await R.pipe(
     findByObjectId,
     R.andThen((result) => console.log(result))
   )('5ef2728266489fa0f8d1f18d', products);
+});
+
+useProductsCollectionOnLocal().then(async (products) => {
+  const result = await findByObjectId('5ef2728266489fa0f8d1f18d', products);
+
+  console.log(result);
+});
+
+useCollectionOnLocalProducts('products').then(async (products) => {
+  const result = await findByObjectId('5ef2728266489fa0f8d1f18d', products);
+
+  console.log(result);
+});
+
+useLocalDatabase('products').then(async (db) => {
+  const result = await findByObjectId('5ef2728266489fa0f8d1f18d', db.collection('products'));
+
+  console.log(result);
 });
